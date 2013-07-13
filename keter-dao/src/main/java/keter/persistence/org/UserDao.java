7ad999f80d8b990fd69496080ffa5c9caa36f1e2
shadowedge <gulixing@msn.com>
2013-07-13 09:41:45 +0800

    Signed-off-by: shadowedge <gulixing@msn.com>

--- keter-dao/src/main/java/keter/persistence/org/UserDao.java
+++
@@ -1,9 +0,0 @@
-package keter.persistence.org;
-
-import keter.domain.User;
-import keter.persistence.base.KeterDao;
-
-public interface UserDao extends KeterDao<User> {
-	
-	User findByAccount(String account);
-}
