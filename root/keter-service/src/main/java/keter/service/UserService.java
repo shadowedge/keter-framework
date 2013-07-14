4ec1eab05a0244183941e4b0ca7615a6418b63b7
shadowedge <gulixing@msn.com>
2013-07-14 16:44:55 +0800

    Signed-off-by: shadowedge <gulixing@msn.com>

---
+++ root/keter-service/src/main/java/keter/service/UserService.java
@@ -0,0 +1,33 @@
+package keter.service;
+
+import java.util.List;
+
+import keter.dao.org.UserDao;
+import keter.domain.User;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service("userService")
+public class UserService {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserDao userDao;
+	
+	public void add(User user){
+		logger.info("add user");
+		userDao.persist(user);
+	}
+	
+	public List<User> all(){
+		logger.info("find all user");
+		return userDao.findAll();
+	}
+
+}
