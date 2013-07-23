aa1e3ba071549c07998537500c853a9aa6c8103b
shadowedge <gulixing@msn.com>
2013-07-23 21:08:30 +0800

    Signed-off-by: shadowedge <gulixing@msn.com>

--- root/keter-dao/src/test/java/keter/dao/UserDaoTest.java
+++ root/keter-dao/src/test/java/keter/dao/UserDaoTest.java
@@ -1,12 +1,15 @@
 package keter.dao;
 
-import junit.framework.Assert;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+//import junit.framework.Assert;
 import keter.KeterAbstractPersistenceTest;
 import keter.dao.org.UserDao;
 import keter.domain.Authority;
 import keter.domain.User;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -52,34 +55,80 @@ public class UserDaoTest extends KeterAbstractPersistenceTest {
 		dao.saveEntity(u1);
 		
 		// 查询：特定
-		Assert.assertEquals("顾", dao.findById(u.getId()).getUsername());
+		assertThat("顾", is(dao.findById(u.getId()).getUsername()));
 		
 		// 查询：特定
-		Assert.assertEquals("gu", dao.findByAccount(u.getAccount()).getAccount());
+		Assert.assertThat("gu", is(dao.findByAccount(u.getAccount()).getAccount()));
 				
 		// 查询：全部
-		Assert.assertEquals(2, dao.findAll().size());
+		Assert.assertThat(2, is(dao.findAll().size()));
 		
 		// 查询：权限（关联）
-		Assert.assertTrue(dao.findById(u.getId()).getAuthorities().contains(Authority.ADMIN));
+		Assert.assertThat(dao.findById(u.getId()).getAuthorities().contains(Authority.ADMIN), is(true));
 		
 		// 修改
 		u.setUsername("杨");
 		dao.saveOrUpdate(u);
-		Assert.assertEquals("杨", dao.findById(u.getId()).getUsername());
+		Assert.assertThat("杨",is(dao.findById(u.getId()).getUsername()));
 		
 		// 删除
 		logger.info("begin delete:");
 		dao.delete(u);
-		Assert.assertEquals(1, dao.findAll().size());
-
-		/*
-		
-		// 查询：特定
-		Assert.assertEquals("顾", dao.findByAccount("gu").getUsername());
-		
-		
-		
+		Assert.assertThat(1,is(dao.findAll().size()));		
+		/**
+		//一般匹配符   
+		   int s = new C().add(1, 1);  
+		   //allOf：所有条件必须都成立，测试才通过   
+		   assertThat(s, allOf(greaterThan(1), lessThan(3)));  
+		   //anyOf：只要有一个条件成立，测试就通过   
+		   assertThat(s, anyOf(greaterThan(1), lessThan(1)));  
+		   //anything：无论什么条件，测试都通过   
+		   assertThat(s, anything());  
+		   //is：变量的值等于指定值时，测试通过   
+		   assertThat(s, is(2));  
+		   //not：和is相反，变量的值不等于指定值时，测试通过   
+		   assertThat(s, not(1));  
+		  
+		   //数值匹配符   
+		   double d = new C().div(10, 3);  
+		   //closeTo：浮点型变量的值在3.0±0.5范围内，测试通过   
+		   assertThat(d, closeTo(3.0, 0.5));  
+		   //greaterThan：变量的值大于指定值时，测试通过   
+		   assertThat(d, greaterThan(3.0));  
+		   //lessThan：变量的值小于指定值时，测试通过   
+		   assertThat(d, lessThan(3.5));  
+		   //greaterThanOrEuqalTo：变量的值大于等于指定值时，测试通过   
+		   assertThat(d, greaterThanOrEqualTo(3.3));  
+		   //lessThanOrEqualTo：变量的值小于等于指定值时，测试通过   
+		   assertThat(d, lessThanOrEqualTo(3.4));  
+		  
+		   //字符串匹配符   
+		   String n = new C().getName("Magci");  
+		   //containsString：字符串变量中包含指定字符串时，测试通过   
+		   assertThat(n, containsString("ci"));  
+		   //startsWith：字符串变量以指定字符串开头时，测试通过   
+		   assertThat(n, startsWith("Ma"));  
+		   //endsWith：字符串变量以指定字符串结尾时，测试通过   
+		   assertThat(n, endsWith("i"));  
+		   //euqalTo：字符串变量等于指定字符串时，测试通过   
+		   assertThat(n, equalTo("Magci"));  
+		   //equalToIgnoringCase：字符串变量在忽略大小写的情况下等于指定字符串时，测试通过   
+		   assertThat(n, equalToIgnoringCase("magci"));  
+		   //equalToIgnoringWhiteSpace：字符串变量在忽略头尾任意空格的情况下等于指定字符串时，测试通过   
+		   assertThat(n, equalToIgnoringWhiteSpace(" Magci   "));  
+		  
+		   //集合匹配符   
+		   List<String> l = new C().getList("Magci");  
+		   //hasItem：Iterable变量中含有指定元素时，测试通过   
+		   assertThat(l, hasItem("Magci"));  
+		  
+		   Map<String, String> m = new C().getMap("mgc", "Magci");  
+		   //hasEntry：Map变量中含有指定键值对时，测试通过   
+		   assertThat(m, hasEntry("mgc", "Magci"));  
+		   //hasKey：Map变量中含有指定键时，测试通过   
+		   assertThat(m, hasKey("mgc"));  
+		   //hasValue：Map变量中含有指定值时，测试通过   
+		   assertThat(m, hasValue("Magci"));  
 		*/
 	}
 }
